fb2743cc7ebd5af22be79aecf6a0961cdcea76ba
Anh Khang <taicuoi664@gmail.com>
2024-08-08 09:19:39 +0700

    Add Phan03_Bai02.1_Tìm hỗn số x trong mảng b theo giải thuật Linear Search.

---
+++ Buoi02/Phan03_Bai02.cpp
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <stdlib.h>
+#define MAXSIZE 100
+
+struct HonSo
+{
+	int phanNguyen, tuSo, mauSo;
+};
+
+void tao_HonSo(HonSo a[], int &n)
+{
+	printf("Nhap so luong phan tu:");
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		a[i].phanNguyen=rand()%10;
+		a[i].tuSo=rand()%10;
+		int t=rand()%10;
+		if (t!=0)
+		{
+			a[i].mauSo=t;
+		}
+		else t=1;
+	}
+}
+
+void xuat_HonSo(HonSo a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("Hon so thu %d: %d va %d/%d\n", i+1,a[i].phanNguyen, a[i].tuSo, a[i].mauSo);
+	}
+}
+
+int SoSanh_HonSo(HonSo a, HonSo b)
+{
+	if (a.phanNguyen>b.phanNguyen)
+	{
+		return 1;
+	}
+	else if (a.phanNguyen<b.phanNguyen)
+	{
+		return -1;
+	}
+	else if (a.phanNguyen==b.phanNguyen)
+	{
+		int tong1 = (float) a.tuSo / a.mauSo;
+		int tong2 = (float) b.tuSo / b.mauSo;
+		if (tong1>tong2)
+		{
+			return 1;
+		}
+		else if (tong1<tong2)
+		{
+			return -1;
+		}
+		else return 0;
+	}
+}
+
+void swap(HonSo &a, HonSo &b)
+{
+	HonSo temp = a;
+	a = b;
+	b = temp;
+}
+
+int tim_Kiem_LinearSearch(HonSo a[], int n, HonSo x)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (SoSanh_HonSo(x,a[i])==0)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+	HonSo a[MAXSIZE];
+	int n;
+	tao_HonSo(a,n);
+	xuat_HonSo(a,n);
+	printf("\n----------------------------------\n");
+	printf("Ta co hon so: %d va %d/%d\n", a[1].phanNguyen, a[1].tuSo, a[1].mauSo);
+	printf("Tim kiem hon so tren trong mang\n");
+	if (tim_Kiem_LinearSearch(a,n,a[1])!=-1)
+	{
+		printf("=>>Da tim thay!!!");
+	}
+	else printf("=>>Khong tim thay!!!");
+
+	getch();
+	return 0;
+}

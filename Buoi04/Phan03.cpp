41769a8071102410a4db68369cf072742dfed6c8
Anh Khang <taicuoi664@gmail.com>
2024-08-08 09:50:56 +0700

    Add Phan03_Bai03_Viết hàm xóa những khoảng trắng thừa trong chuỗi (hay còn gọi là chuẩn hóa chuỗi).

--- Buoi04/Phan03.cpp
+++ Buoi04/Phan03.cpp
@@ -54,6 +54,37 @@ void bienDoi_Chuoi(char s[])
     }
 }
 
+void chuanHoa_Chuoi(char str[]) 
+{
+    int x = 0;
+    int spaceFlag = 0;
+    for (int i = 0; str[i]; i++) 
+	{
+        if (!isspace(str[i])) 
+		{
+            if (spaceFlag && x > 0) 
+			{
+                str[x++] = ' ';
+            }
+            str[x++] = str[i];
+            spaceFlag = 0;
+        } 
+		else if (x > 0)
+		{
+            spaceFlag = 1;
+        }
+    }
+	str[x] = '\0';
+
+    if (isspace(str[0])) 
+	{
+        for (int i = 0; str[i]; i++)
+		{
+            str[i] = str[i + 1];
+        }
+    }
+}
+
 
 int main()
 {
@@ -71,6 +102,10 @@ int main()
 	printf("\n-------------------------------------------------\n");
 	bienDoi_Chuoi(s);
 	printf("Chuoi sau khi bien doi:\n");
+	xuat_Chuoi(s);
+	printf("\n-------------------------------------------------\n");
+	printf("Chuoi sau khi chuan hoa:\n");
+	chuanHoa_Chuoi(s);
 	xuat_Chuoi(s);
 
 	getch();

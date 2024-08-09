913a7cf1ee9c7ce1a21387e33c2733cd9300a975
Le Tan Phu <phule13112004@gmail.com>
2024-08-09 21:18:42 +0700

    Add Phan4_Bai3.1.cpp

---
+++ Buoi03/Phan4_Bai3.cpp
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<math.h>
+void TaoM2C(int a[][100], int& n, int& m) {
+	printf("Moi ban nhap so dong:");
+	scanf("%d", &n);
+	printf("Moi ban nhap so cot:");
+	scanf("%d", &m);
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++) {
+			a[i][j] = rand() % 100;
+		}
+	}
+}
+void XuatM2C(int a[][100], int n, int m) {
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++) {
+			printf("%d\t", a[i][j]);
+		}
+		printf("\n");
+	}
+}
+void XuatCotLe(int a[][100], int n, int m) {
+	for (int i = 0; i < m; i++)
+	{
+		int kt = 1;
+		for (int j = 0; j < n; j++)
+		{
+			if (a[j][i] % 2 == 0) { kt = 0; break; }
+		}
+		if (kt == 1) {
+			printf("Cot %d chi chua so le:", i + 1);
+			for (int k = 0; k < n; k++)
+			{
+				printf("%d\t", a[k][i]);
+			}
+			printf("\n");
+		}
+	}
+}
+int main() {
+	int a[100][100];
+	int n, m;
+	TaoM2C(a, n, m);
+	XuatM2C(a, n, m);
+	XuatCotLe(a, n, m);
+}
